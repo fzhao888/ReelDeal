2eed871eaf433829228e857f6bca511a891023d7
fzhao888 <frank.zhao93@gmail.com>
2023-06-13 19:25:13 -0500

    Edited JavaScript so the image is on the left and the text is on the right, also styled the save button

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -12,12 +12,45 @@ if (!storedMovies) {
         var resultCard = document.createElement("div");
         //add bulma css
         resultCard.classList.add("card");
+
+        var resultHeader = document.createElement("header");
+        resultHeader.classList.add("card-header");
+        resultCard.append(resultHeader);
+
+        var movieNameEl = document.createElement("p");
+        movieNameEl.classList.add("card-header-title");
+        movieNameEl.textContent = movie.Title;
+        movieNameEl.style.justifyContent = "center";
+        resultHeader.append(movieNameEl);
+
+
+
         var resultBody = document.createElement("div");
-        resultBody.classList.add("card-body");
+        resultBody.classList.add("card-content");
+        resultBody.style.fontSize = "large";
         resultCard.append(resultBody);
 
-        var movieNameEl = document.createElement("h3");
-        movieNameEl.textContent = movie.Title;
+
+        var mediaDiv = document.createElement("div");
+        mediaDiv.classList.add("media");
+        resultBody.append(mediaDiv);
+
+        var leftImageDiv = document.createElement("div");
+        leftImageDiv.classList.add("media-left");
+        mediaDiv.append(leftImageDiv);
+
+
+        var imageFigure = document.createElement("figure");
+        imageFigure.classList.add("image");
+        leftImageDiv.append(imageFigure);
+
+        var imageEl = document.createElement('img');
+        imageEl.src = movie.Poster;
+        imageFigure.append(imageEl);
+
+        var mediaContent = document.createElement("div");
+        mediaContent.classList.add("media-content");
+        mediaDiv.append(mediaContent);
 
         var releaseYearEl = document.createElement('p');
         releaseYearEl.innerHTML = '<strong> Release Year: </strong> ' + movie.Year + '</br>';
@@ -34,14 +67,15 @@ if (!storedMovies) {
         var plotEl = document.createElement('p');
         plotEl.innerHTML = '<strong> Plot: </strong> ' + movie.Plot + '</br>';
 
-        var imageEl = document.createElement('img');
-        imageEl.src = movie.Poster;
 
         var ratingsEl = document.createElement('p');
         ratingsEl.innerHTML = '<strong> IMDB Rating: </strong> ' + movie.Ratings[0].Value + '</br>';
 
-        resultBody.append(movieNameEl, imageEl, ratingsEl, releaseYearEl, ratedEl, runtimeEl, genreEl, plotEl);
+        mediaContent.style.textAlign = "left";
+        mediaContent.append(ratingsEl, releaseYearEl, ratedEl, runtimeEl, genreEl, plotEl);
+
         favoritesContentEl.append(resultCard);
+
     }
 }
 
